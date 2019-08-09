aa6e5ca52ce82cbbcd0597c7624cb89849fb3588
Daphne Laboul <daphne.laboul@gmail.com>
2019-08-09 18:40:08 +0200

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index', as: :cocktails
   get 'cocktails/new', to: 'cocktails#new', as: :new_cocktail
-  post root , to: 'cocktails#create'
+  post 'cocktails', to: 'cocktails#create'
   get 'cocktails/:id', to: 'cocktails#show', as: :cocktail
   get 'cocktails/:id/doses/new', to: 'doses#new', as: :new_dose
   post 'cocktails/:id/doses', to: 'doses#create', as: :doses
