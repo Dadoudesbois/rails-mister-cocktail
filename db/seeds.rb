d6ebe5a428fdbf16eff1228dd2c84c65831eb6f0
Daphne Laboul <daphne.laboul@gmail.com>
2019-08-09 11:06:50 +0200

    ing

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-ingredients = Ingredient.create([{ name: "Lavender liquor" }, { name: "Violette" }, { name: "Ginger" }, { name: "Tequilla" }, { name: "Basil" }, { name: "Cucumber" }, { name: "Cinnamon" }, { name: "Rhum" }, { name: "Gin" }, { name: "Agave syrup" }, { name: "Cachaca" }, { name: "Grand Marnier" }, { name: "Amaretto" }])
+# ingredients = Ingredient.create([{ name: "Lavender liquor" }, { name: "Violette" }, { name: "Ginger" }, { name: "Tequilla" }, { name: "Basil" }, { name: "Cucumber" }, { name: "Cinnamon" }, { name: "Rhum" }, { name: "Gin" }, { name: "Agave syrup" }, { name: "Cachaca" }, { name: "Grand Marnier" }, { name: "Amaretto" }])
+
+require 'open-uri'
+ingredients = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
+json = JSON.parse(ingredients.read)
+json["drinks"].each { |ingredient| Ingredient.create(name: ingredient["strIngredient1"]) }
